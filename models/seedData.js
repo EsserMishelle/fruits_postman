ec5f1480e2bc3b298c2190588f5944119b997c52
M Esser <essermishelle@gmail.com>
2024-02-09 14:08:49 -0500

    first try

---
+++ models/seedData.js
@@ -0,0 +1,24 @@
+const startFruits = [
+  { name: "Orange", color: "neon", readyToEat: true, username: "Mishelle" },
+  { name: "Grape", color: "purplse", readyToEat: true, username: "Mishelle" },
+  { name: "Banana", color: "orange", readyToEat: false, username: "Mishelle" },
+  { name: "Orange", color: "neon", readyToEat: true, username: "Devin" },
+  { name: "Grape", color: "purplse", readyToEat: true, username: "Devin" },
+  { name: "Banana", color: "orange", readyToEat: false, username: "Devin" },
+
+  {
+    name: "Raspberry",
+    color: "orange",
+    readyToEat: false,
+    username: "Mishelle",
+  },
+  {
+    name: "Strawberry",
+    color: "red",
+    readyToEat: false,
+    username: "Mishelle2",
+  },
+  { name: "Coconut", color: "brown", readyToEat: false, username: "Mishelle2" },
+];
+
+module.exports = startFruits;
